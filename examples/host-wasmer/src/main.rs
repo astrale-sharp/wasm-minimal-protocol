ff974aad8ee3f1bf603084915bc330887f947146
astrale-sharp <ash4567@outlook.fr>
2023-07-02 14:35:27 +0000

    adds support for error report from the host adds support for using the host as a library TODO, accept signature Result<String, String> for the macro, and returning code != 0

--- examples/host-wasmer/src/main.rs
+++ examples/host-wasmer/src/main.rs
@@ -1,8 +1,10 @@
 // this is an example of host
 // you need to build the hello example first
 
+use std::sync::{Arc, Mutex};
+
 use anyhow::Result;
-use wasmer::{imports, Instance, Module, Store, Value};
+use wasmer::{imports, Instance, Memory, Module, Store, Value};
 
 fn main() -> Result<()> {
     let mut store = Store::default();
@@ -13,7 +15,7 @@ fn main() -> Result<()> {
     let import_object = imports! {};
     let instance = Instance::new(&mut store, &module, &import_object)?;
 
-    let mut plugin_instance = PluginInstance::new(&instance, &mut store);
+    let mut plugin_instance = PluginInstance::new(instance, store);
 
     println!("{:?}", plugin_instance.call("hello", &[]));
     println!("{:?}", plugin_instance.call("double_it", &["double me!!"]));
@@ -29,78 +31,130 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-struct PluginInstance<'a> {
-    instance: &'a Instance,
-    store: &'a mut Store,
-    allocate_storage: &'a wasmer::Function,
-    get_storage_pointer: &'a wasmer::Function,
-    get_storage_len: &'a wasmer::Function,
-    memory: &'a wasmer::Memory,
+pub struct PluginInstance {
+    store: Arc<Mutex<Store>>,
+    memory: Arc<Mutex<Memory>>,
+    allocate_storage: wasmer::Function,
+    get_storage_pointer: wasmer::Function,
+    get_storage_len: wasmer::Function,
+    functions: Vec<(String, wasmer::Function)>,
 }
 
-impl<'a> PluginInstance<'a> {
-    fn new(instance: &'a Instance, store: &'a mut Store) -> Self {
+impl PluginInstance {
+    pub fn new(instance: Instance, store: Store) -> Self {
         // important functions that we will often use.
         let allocate_storage = instance
             .exports
             .get_function("wasm_minimal_protocol_allocate_storage")
-            .unwrap();
+            .unwrap()
+            .clone();
         let get_storage_pointer = instance
             .exports
             .get_function("wasm_minimal_protocol_get_storage_pointer")
-            .unwrap();
+            .unwrap()
+            .clone();
         let get_storage_len = instance
             .exports
             .get_function("wasm_minimal_protocol_get_storage_len")
-            .unwrap();
-        let memory = instance.exports.get_memory("memory").unwrap();
+            .unwrap()
+            .clone();
+
+        let memory = instance.exports.get_memory("memory").unwrap().clone();
+
+        let functions = instance
+            .exports
+            .iter()
+            .filter_map(|(s, e)| match e {
+                wasmer::Extern::Function(f) => Some((s.to_owned(), f.clone())),
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+        // .map_err(|_| "")
+        // .to_owned();
+
         Self {
-            instance,
-            store,
+            store: Arc::new(Mutex::new(store)),
+            memory: Arc::new(Mutex::new(memory)),
             allocate_storage,
             get_storage_pointer,
             get_storage_len,
-            memory,
+            functions,
         }
     }
 
+    fn store(&self) -> Result<std::sync::MutexGuard<'_, wasmer::Store>, String> {
+        self.store
+            .lock()
+            .map_err(|_| "Couldn't lock the plugin store".into())
+    }
+
+    fn memory(&self) -> Result<std::sync::MutexGuard<'_, wasmer::Memory>, String> {
+        self.memory
+            .lock()
+            .map_err(|_| "Couldn't lock the plugin memory".into())
+    }
+
     /// Write arguments in `__RESULT`.
-    fn write(&mut self, args: &[&str]) {
+    fn write(&mut self, args: &[&str]) -> Result<(), String> {
         let total_len = args.iter().map(|a| a.len()).sum::<usize>();
         self.allocate_storage
-            .call(self.store, &[Value::I32(total_len as _)])
+            .call(&mut self.store()?, &[wasmer::Value::I32(total_len as _)])
             .unwrap();
-        let mut storage_pointer =
-            self.get_storage_pointer.call(self.store, &[]).unwrap()[0].unwrap_i32() as u64;
+        let mut storage_pointer = self
+            .get_storage_pointer
+            .call(&mut self.store()?, &[])
+            .unwrap()[0]
+            .unwrap_i32() as u64;
         for arg in args {
-            self.memory
-                .view(self.store)
+            self.memory()?
+                .view(&self.store()?)
                 .write(storage_pointer, arg.as_bytes())
                 .unwrap();
             storage_pointer += arg.len() as u64;
         }
+        Ok(())
     }
 
-    fn call(&mut self, function: &str, args: &[&str]) -> String {
-        self.write(args);
-        let args = args
+    pub fn call(&mut self, function: &str, args: &[&str]) -> Result<String, String> {
+        self.write(args)?;
+
+        let (_, function) = self
+            .functions
             .iter()
-            .map(|a| Value::I32(a.len() as _))
+            .find(|(s, _)| s == function)
+            .ok_or("Plugin doesn't have the method: {function}")?;
+
+        let result_args = args
+            .iter()
+            .map(|a| wasmer::Value::I32(a.len() as _))
             .collect::<Vec<_>>();
 
-        let function = self.instance.exports.get_function(function).unwrap();
-        function.call(self.store, &args).unwrap();
+        let code = &function.call(&mut self.store()?, &result_args).unwrap()[0];
 
         // Get the resulting string in `__RESULT`
-        let storage_pointer =
-            self.get_storage_pointer.call(self.store, &[]).unwrap()[0].unwrap_i32() as u64;
-        let len = self.get_storage_len.call(self.store, &[]).unwrap()[0].unwrap_i32();
+        let storage_pointer = self
+            .get_storage_pointer
+            .call(&mut self.store()?, &[])
+            .unwrap()[0]
+            .unwrap_i32() as u64;
+        let len = self.get_storage_len.call(&mut self.store()?, &[]).unwrap()[0].unwrap_i32();
 
         let mut result = vec![0u8; len as usize];
-        self.memory
-            .view(self.store)
+        self.memory()?
+            .view(&self.store()?)
             .read(storage_pointer, &mut result)
             .unwrap();
-        String::from_utf8(result).unwrap()
+
+        let s = String::from_utf8(result).map_err(|_| "Plugin data is not utf8".into());
+
+        if code != &Value::I32(0) {
+            Err(format!(
+                "plugin errored with: {:?} with code: {}",
+                s,
+                code.i32().unwrap()
+            ))
+        } else {
+            s
+        }
     }
 }
