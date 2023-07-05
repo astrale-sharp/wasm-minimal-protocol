84ce313d66c3e8a7228d24983d22b33ae51f9acf
Arnaud Golfouse <53786772+arnaudgolfouse@users.noreply.github.com>
2023-07-05 19:09:13 +0200

    Implement the rest of the C example (#7)

--- examples/hello_c/hello.c
+++ examples/hello_c/hello.c
@@ -40,8 +40,89 @@ int32_t double_it(size_t arg_len) {
   return 0;
 }
 
-// TODO: 'concatenate' function
-// TODO: 'shuffle' function
-// TODO: 'returns_ok' function
-// TODO: 'returns_err' function
-// TODO: 'will_panic' function
+EMSCRIPTEN_KEEPALIVE
+int32_t concatenate(size_t arg1_len, size_t arg2_len) {
+  size_t total_len = arg1_len + arg2_len;
+  uint8_t *args = (uint8_t *)malloc(total_len);
+  uint8_t *result = (uint8_t *)malloc(total_len + 1);
+  if (args == NULL) {
+    return 1;
+  } else if (result == NULL) {
+    free(args);
+    return 1;
+  }
+
+  wasm_minimal_protocol_write_args_to_buffer(args);
+  uint8_t *arg1 = args;
+  uint8_t *arg2 = args + arg1_len;
+
+  for (size_t i = 0; i < arg1_len; i++) {
+    result[i] = arg1[i];
+  }
+  result[arg1_len] = '*';
+  for (size_t i = 0; i < arg2_len; i++) {
+    result[arg1_len + 1 + i] = arg2[i];
+  }
+
+  wasm_minimal_protocol_send_result_to_host(result, total_len + 1);
+
+  free(result);
+  free(args);
+  return 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int32_t shuffle(size_t arg1_len, size_t arg2_len, size_t arg3_len) {
+  size_t result_len = arg1_len + arg2_len + arg3_len + 2;
+  uint8_t *args = (uint8_t *)malloc(arg1_len + arg2_len + arg3_len);
+  uint8_t *result = (uint8_t *)malloc(result_len);
+  if (args == NULL) {
+    return 1;
+  } else if (result == NULL) {
+    free(args);
+    return 1;
+  }
+
+  wasm_minimal_protocol_write_args_to_buffer(args);
+  uint8_t *arg1 = args;
+  uint8_t *arg2 = args + arg1_len;
+  uint8_t *arg3 = args + arg1_len + arg2_len;
+
+  for (size_t i = 0; i < arg3_len; i++) {
+    result[i] = arg3[i];
+  }
+  result[arg3_len] = '-';
+  for (size_t i = 0; i < arg1_len; i++) {
+    result[arg3_len + 1 + i] = arg1[i];
+  }
+  result[arg3_len + arg1_len + 1] = '-';
+  for (size_t i = 0; i < arg2_len; i++) {
+    result[arg3_len + arg1_len + 2 + i] = arg2[i];
+  }
+
+  wasm_minimal_protocol_send_result_to_host(result, result_len);
+
+  free(result);
+  free(args);
+  return 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int32_t returns_ok() {
+  const char message[] = "This is an `Ok`";
+  wasm_minimal_protocol_send_result_to_host((uint8_t *)message,
+                                            sizeof(message) - 1);
+  return 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int32_t returns_err() {
+  const char message[] = "This is an `Err`";
+  wasm_minimal_protocol_send_result_to_host((uint8_t *)message,
+                                            sizeof(message) - 1);
+  return 1;
+}
+
+// Needs WASI
+// EMSCRIPTEN_KEEPALIVE
+// int32_t will_panic() { exit(1); }
